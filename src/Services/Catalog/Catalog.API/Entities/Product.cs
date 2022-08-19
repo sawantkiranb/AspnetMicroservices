bee3fd38fb1db447c2d0723b0805e1b9236b7ed4
sawantkiranb <sawant.kiran.b@gmail.com>
2022-08-19 14:40:05 +0530

    Developing data layer for Catalog API

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -14,7 +14,7 @@ namespace Catalog.API.Entities
         public string Summary { get; set; }
         public string Description { get; set; }
         public string ImageFile { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
 
     }
 }
